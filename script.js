40847c0e80c7bdeb2ae2ac9ddf73f96540703b7b
Shivam1456 <127660326+Shivam1456@users.noreply.github.com>
2023-08-15 18:29:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 //your code here
 
+const bandNames = [
+  "The Beatles",
+  "Led Zeppelin",
+  "Nirvana",
+  "The Rolling Stones",
+  "Aerosmith",
+  "The Who",
+  "Queen",
+  "Anthrax",
+  "The Clash"
+];
+
+function removeArticles(name) {
+  return name.replace(/^(a |an |the )/i, '').trim();
+}
+
+bandNames.sort((a, b) => removeArticles(a).localeCompare(removeArticles(b)));
+
+const bandList = document.getElementById('band');
+
+for (let band of bandNames) {
+  const listItem = document.createElement('li');
+  listItem.textContent = band;
+  bandList.appendChild(listItem);
+}
