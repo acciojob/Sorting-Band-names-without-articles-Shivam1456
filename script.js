5aa06870cf360697e654d733aa33008980a41018
Shivam1456 <127660326+Shivam1456@users.noreply.github.com>
2023-08-15 18:33:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //your code here
 
+// Array of band names
 const bandNames = [
   "The Beatles",
   "Led Zeppelin",
@@ -12,14 +13,18 @@ const bandNames = [
   "The Clash"
 ];
 
+// Function to remove articles from band name
 function removeArticles(name) {
   return name.replace(/^(a |an |the )/i, '').trim();
 }
 
+// Sort band names
 bandNames.sort((a, b) => removeArticles(a).localeCompare(removeArticles(b)));
 
-const bandList = document.getElementById('band');
+// Get the ul element by its ID
+const bandList = document.querySelector('#bands');
 
+// Populate the ul with sorted band names
 for (let band of bandNames) {
   const listItem = document.createElement('li');
   listItem.textContent = band;
